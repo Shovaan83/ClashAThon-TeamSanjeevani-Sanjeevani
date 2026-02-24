55cc764f62b62e0c995dc63857a4dd53c107cece
Shovaan83 <shovanbthr@gmail.com>
2026-02-24 22:51:59 +0545

    Completion of Pharmacy Pages frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,6 +7,7 @@ import PatientDashboardPage from './features/home/pages/PatientDashboardPage';
 import PharmacyDashboardPage from './features/home/pages/PharmacyDashboardPage';
 import PatientProfilePage from './features/home/pages/PatientProfilePage';
 import PharmacyProfile from "./features/home/pages/PharmacyProfile";
+import SearchPage from "./features/home/pages/SearchPage";
 import DevTools from "./components/DevTools";
 import { useAuthStore } from "./store/useAuthStore";
 
@@ -30,8 +31,14 @@ export default function App() {
         {/* Landing */}
         <Route path="/home" element={<HomePage />} />
 
+        {/* Pharmacy search */}
+        <Route path="/search" element={<SearchPage />} />
+
         {/* Pharmacy Profile */}
         <Route path="/pharmacy/:id" element={<PharmacyProfile />} />
+
+        {/* Broadcast (patient ping flow) */}
+        <Route path="/broadcast" element={<PatientDashboardPage />} />
 
         {/* Role-specific dashboards */}
         <Route path="/dashboard/patient" element={<PatientDashboardPage />} />
